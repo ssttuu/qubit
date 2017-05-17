bc79a880bbc916ccaa79f1db20c3d7f555413288
Stu Schwartz <stupschwartz@gmail.com>
2017-05-16 20:34:44 -0400

    update deployment output text

--- compute/deploy.sh
+++ compute/deploy.sh
@@ -12,12 +12,11 @@ pushd `dirname $0`
 protoc -I /usr/local/include/ -I ./ --include_imports --include_source_info compute.proto --descriptor_set_out compute.pb
 
 # Deploy Compute Service
+echo "Deploying to Google Endpoints"
 service_response=$(gcloud service-management deploy compute.pb api_config.yaml --format json)
 
 service_id=$(echo ${service_response} | jq -r '.serviceConfig.id')
 service_name=$(echo ${service_response} | jq -r '.serviceConfig.name')
-
-echo "Deployment: ${service_id} of ${service_name}"
 
 deployment_formatted="k8s/compute.deployment.formatted.yaml"
 
@@ -25,4 +24,5 @@ deployment_formatted="k8s/compute.deployment.formatted.yaml"
 jinja2 k8s/compute.deployment.yaml -D id=${service_id} -D name=${service_name} -D githash=$(git rev-parse HEAD) > ${deployment_formatted}
 
 # Create deployment using JSON configuration
+echo "Deploying to Kubernetes"
 kubectl create -f ${deployment_formatted}
