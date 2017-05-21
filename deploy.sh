d3c983c9a3c82d0b5b04ab6bc26e97bab9e53922
Stu Schwartz <stupschwartz@gmail.com>
2017-05-21 18:34:21 -0400

    make protos before deploying (to be fixed)

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,9 @@
 
 set -eo pipefail
 
-# Create Compute Service Description
-docker run --rm -v $(pwd):/workspace -w /workspace znly/protoc --include_imports --include_source_info compute/compute.proto --descriptor_set_out compute/compute.pb
-
 # Deploy Compute Service
 echo "Deploying to Google Endpoints"
-service_response=$(gcloud service-management deploy compute/compute.pb compute/api_config.yaml --format json)
+service_response=$(gcloud service-management deploy compute/protos/compute/compute.pb compute/api_config.yaml --format json)
 
 service_id=$(echo ${service_response} | jq -r '.serviceConfig.id')
 service_name=$(echo ${service_response} | jq -r '.serviceConfig.name')
