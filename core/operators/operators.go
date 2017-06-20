b73323726a1447e75961d3a55536790f7b46023b
Stu Schwartz <stupschwartz@gmail.com>
2017-06-20 07:20:58 -0400

    roto parameters

--- core/operators/operators.go
+++ core/operators/operators.go
@@ -3,4 +3,5 @@ package operators
 import (
 	_ "github.com/stupschwartz/qubit/core/operators/checkerboard"
 	_ "github.com/stupschwartz/qubit/core/operators/constant"
+	_ "github.com/stupschwartz/qubit/core/operators/roto"
 )
